e1a8fe2d378b6e728ad8f0e204eb14308f5ae0e9
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-03 16:14:08 +0000

    Add interactive script for hamburger menu

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(link => link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+}));
