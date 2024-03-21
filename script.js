2fca9ee833b6a18addeb7744b506e33eb58695d9
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-21 11:15:59 +0000

    Refactor event listeners and add transition to links

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navLinks.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav-link").forEach(link => link.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navLinks.classList.remove("active");
+document.querySelectorAll('.nav-link').forEach(link => link.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navLinks.classList.remove('active');
 }));
